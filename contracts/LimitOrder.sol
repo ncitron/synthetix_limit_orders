f0c6e54c04a34b07cababb9b6177472c26bc9968
Noah Citron <noah@jeff.org>
2020-10-10 21:22:30 -0400

    implemented placing orders

--- contracts/LimitOrder.sol
+++ contracts/LimitOrder.sol
@@ -4,6 +4,7 @@ pragma experimental ABIEncoderV2;
 
 import "synthetix/contracts/interfaces/IAddressResolver.sol";
 import "synthetix/contracts/interfaces/IExchangeRates.sol";
+import "synthetix/contracts/interfaces/ISynth.sol";
 
 
 contract LimitOrder {
@@ -18,25 +19,39 @@ contract LimitOrder {
     Order[] orders;
     IAddressResolver resolver;
     IExchangeRates exchangeRates;
+    ISynth sUSD;
+    ISynth sBTC;
 
     constructor(IAddressResolver _resolver) {
         resolver =_resolver;
         address addr = resolver.getAddress('ExchangeRates');
         exchangeRates = IExchangeRates(addr);
+        sUSD = ISynth(resolver.getSynth('sUSD'));
+        sBTC = ISynth(resolver.getSynth('sBTC'));
     }
 
     function getPrice() public view returns (uint256) {
         return exchangeRates.rateForCurrency('sBTC');
     }
 
-    function placeOrder(bool _isBuy, uint256 _amount, uint256 _price) external payable {
-        require(_amount < 0 || _price < 0, 'amount and price must be positive values');
-        //TODO: transfer custody of funds to contract
-        orders.push(Order({user: msg.sender, isBuy: _isBuy, amount: _amount, price: _price}));
+    function placeBuyOrder(uint256 _amount, uint256 _price) external payable {
+        require(_amount > 0 || _price > 0, 'amount and price must be positive values');
+        sUSD.transferFromAndSettle(msg.sender, address(this), _amount);
+        orders.push(Order({user: msg.sender, isBuy: true, amount: _amount, price: _price}));
+    }
+
+    function placeSellOrder(uint256 _amount, uint256 _price) external payable {
+        require(_amount > 0 || _price > 0, 'amount and price must be positive values');
+        sBTC.transferFromAndSettle(msg.sender, address(this), _amount);
+        orders.push(Order({user: msg.sender, isBuy: false, amount: _amount, price: _price}));
     }
 
     function getOrders() public view returns (Order[] memory) {
         return orders;
+    }
+    
+    function cancel() public {
+
     }
 
     function execute() public {
--- test/test.js
+++ test/test.js
@@ -1,30 +1,272 @@
-const should = require("chai").should();
+require("chai").should();
 const snx = require("synthetix");
 
 describe("LimitOrder", function() {
 
-    let deployer, account1, account2;
+    let account0, account1, account2;
     let limitOrder;
+    let sUSD, sBTC;
 
     beforeEach(async () => {
-        [deployer, account1, account2] = await ethers.getSigners();
+        [account0, account1, account2] = await ethers.getSigners();
+
         let proxy = snx.getTarget({ network: 'kovan', contract: 'ReadProxyAddressResolver' }).address;
+
+        let synths = snx.getTokens({network: 'kovan'});
+        sUSD = new ethers.Contract(synths.filter(({ symbol }) => symbol === 'sUSD')[0].address, erc20, account0);
+        sBTC = new ethers.Contract(synths.filter(({ symbol }) => symbol === 'sBTC')[0].address, erc20, account0);
 
         let LimitOrder = await ethers.getContractFactory("LimitOrder");
         limitOrder = await LimitOrder.deploy(proxy);
+        await limitOrder.deployed();
     });
 
     it("Should be able to fetch sBTC price", async () => {
-        await limitOrder.deployed();
         let rate = await limitOrder.getPrice();
         rate.gt(0).should.equal(true);
     });
 
-    it("Should be able to place an order", async () => {
-        
+    it("Should be able to place a buy limit order", async () => {
+        await sUSD.approve(limitOrder.address, 1);
+        let initialBalance = await sUSD.balanceOf(account0.getAddress());
+        await limitOrder.placeBuyOrder(1, 12000);
+        let finalBalance = await sUSD.balanceOf(account0.getAddress());
+        finalBalance.sub(initialBalance).eq(-1).should.equal(true);
     });
 
-    it("Should be able to fetch all orders", async () => {
-
+    it("should be able to place a sell limit order", async () => {
+        await sBTC.approve(limitOrder.address, 1);
+        let initialBalance = await sBTC.balanceOf(account0.getAddress());
+        await limitOrder.placeSellOrder(1, 12000);
+        let finalBalance = await sBTC.balanceOf(account0.getAddress());
+        finalBalance.sub(initialBalance).eq(-1).should.equal(true);
     });
+
+    /*it("should be able to cancel an order", async () => {
+
+    })*/
 });
+
+
+const erc20 = [
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "name",
+        "outputs": [
+            {
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "_spender",
+                "type": "address"
+            },
+            {
+                "name": "_value",
+                "type": "uint256"
+            }
+        ],
+        "name": "approve",
+        "outputs": [
+            {
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "totalSupply",
+        "outputs": [
+            {
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "_from",
+                "type": "address"
+            },
+            {
+                "name": "_to",
+                "type": "address"
+            },
+            {
+                "name": "_value",
+                "type": "uint256"
+            }
+        ],
+        "name": "transferFrom",
+        "outputs": [
+            {
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "decimals",
+        "outputs": [
+            {
+                "name": "",
+                "type": "uint8"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [
+            {
+                "name": "_owner",
+                "type": "address"
+            }
+        ],
+        "name": "balanceOf",
+        "outputs": [
+            {
+                "name": "balance",
+                "type": "uint256"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "symbol",
+        "outputs": [
+            {
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "_to",
+                "type": "address"
+            },
+            {
+                "name": "_value",
+                "type": "uint256"
+            }
+        ],
+        "name": "transfer",
+        "outputs": [
+            {
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [
+            {
+                "name": "_owner",
+                "type": "address"
+            },
+            {
+                "name": "_spender",
+                "type": "address"
+            }
+        ],
+        "name": "allowance",
+        "outputs": [
+            {
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "payable": true,
+        "stateMutability": "payable",
+        "type": "fallback"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": true,
+                "name": "owner",
+                "type": "address"
+            },
+            {
+                "indexed": true,
+                "name": "spender",
+                "type": "address"
+            },
+            {
+                "indexed": false,
+                "name": "value",
+                "type": "uint256"
+            }
+        ],
+        "name": "Approval",
+        "type": "event"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": true,
+                "name": "from",
+                "type": "address"
+            },
+            {
+                "indexed": true,
+                "name": "to",
+                "type": "address"
+            },
+            {
+                "indexed": false,
+                "name": "value",
+                "type": "uint256"
+            }
+        ],
+        "name": "Transfer",
+        "type": "event"
+    }
+]
